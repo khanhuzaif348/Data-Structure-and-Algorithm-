11bb4fad6b81b587b5b4242dcad468915da932d1
khanhuzaif348 <khanhuzaif348@gmail.com>
2025-12-05 21:26:22 +0530

    Cimmit

--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,15 @@
+# IMporting Modules in python : Modules and Package
+''' In Python,modules and packages help organize and reuse code .
+Here's comprehensive guide on how to import them'''
+
+import math 
+math.sqrt(16)# return 4
+from math import sqrt ,pi
+print(sqrt(16))
+print(sqrt(25))
+print(pi)
+
+
 from package import addition
 a  = addition(1,3)
 print(a)
