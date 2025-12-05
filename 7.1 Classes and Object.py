564e75333a0ed55d0e38d4355a2315c669a31786
khanhuzaif348 <khanhuzaif348@gmail.com>
2025-12-05 22:33:36 +0530

    commit

---
+++ 7.1 Classes and Object.py
@@ -0,0 +1,13 @@
+# Class And Object 
+
+'''OOPs to design application and computer programs. 
+OOP allows for modelling real worldscenario using class and Object, 
+This lesson cover the basics of creating class and objects incluisng instance variables and methods
+''''
+
+
+
+class car:
+    pass
+a
+    
